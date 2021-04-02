d8ed8a9334c67d56ba19e4a6b7d78bdfa482d8cb
Tim Flood <tflood9@gmail.com>
2021-04-02 13:21:02 -0400

    commit tree 77e3a3f2cec9cb1abb5c82ebffc8f0ef7813133a

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h3>Name: {movie.title}</h3>
+          <h3>Time: {movie.time}</h3>
+          <h3>Genre: {movie.genre}</h3>
+          <ul>
+            {movie.genres.map((genre, index) => (
+              <li key={index}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
